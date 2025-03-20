84c8e68cef5ce78ae51f1bb18d77b5aa7dd2d736
lisa biltiaieva <b44800395@gmail.com>
2025-03-20 20:17:44 +0000

    task4

---
+++ task4.py
@@ -0,0 +1,19 @@
+
+#task 4
+
+print("What's your mark?")
+mark = int(input())
+if 0 <= mark <= 49:
+    print("You did badly")
+elif 50 <= mark <= 69:
+    print('You did ok')
+elif 70 <= mark <= 89:
+    print('You did good!')
+elif 90 <= mark <= 100:
+    print("Amazing!")
+
+
+
+
+
+
